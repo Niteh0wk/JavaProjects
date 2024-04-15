ae02c64c7e6b192a58731fae813b448c99346c0b
Niteh0wk <gerstl.philipp.24@gmail.com>
2024-04-15 13:53:39 +0200

    Added Kompetenzcheck

---
+++ Kompetenzcheck-CleanCode/src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input;
+
+        input = sc.nextLine();
+
+        for (int x = 0; x < input.length(); x++) {
+            switch (input.charAt(x)) {
+                case 'A':
+                    System.out.print("@");
+                    break;
+                case 'B':
+                    System.out.print("8");
+                    break;
+                case 'C':
+                    System.out.print("(");
+                    break;
+                case 'E':
+                    System.out.print("3");
+                    break;
+                case 'G':
+                    System.out.print("6");
+                    break;
+                case 'H':
+                    System.out.print("#");
+                    break;
+                case 'I':
+                    System.out.print("!");
+                    break;
+                case 'L':
+                    System.out.print("1");
+                    break;
+                case 'O':
+                    System.out.print("0");
+                    break;
+                case 'S':
+                    System.out.print("$");
+                    break;
+                case 'T':
+                    System.out.print("7");
+                    break;
+                case 'Z':
+                    System.out.print("2");
+                    break;
+                default:
+                    System.out.print(input.charAt(x));
+            }
+        }
+    }
+
+}
