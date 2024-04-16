bdce660eaa0b57c0aa7ffce0d24c5455a53a0289
Niteh0wk <gerstl.philipp.24@gmail.com>
2024-04-16 08:38:56 +0200

    Updated

---
+++ 07-ArraysUsw/src/InsertionBubbleSort.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class InsertionBubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+
+        int arrayLenght;
+        System.out.println("How many numbers should be sorted?");
+        arrayLenght = sc.nextInt();
+        int[] arr = new int[arrayLenght];
+
+        for (int x = 0; x < arr.length; x++) {
+            arr[x] = r.nextInt(101);
+        }
+        int bubbleOrInsertion;
+        do {
+            System.out.println("Bubble or Insertion Sort?(1 or 2)");
+            bubbleOrInsertion = sc.nextInt();
+        }while (bubbleOrInsertion <= 0 || bubbleOrInsertion >= 3);
+
+
+        System.out.println("Unsorted: " + Arrays.toString(arr));
+        if (bubbleOrInsertion == 1){
+            insertionSort(arr);
+        }else if (bubbleOrInsertion == 2){
+            bubble_sort(arr, arrayLenght);
+        }
+
+        System.out.println("Sorted: " + Arrays.toString(arr));
+    }
+
+    static void insertionSort(int[] sorted) {
+        int n = sorted.length;
+        for (int i = 1; i < n; ++i) {
+            int key = sorted[i];
+            int j = i - 1;
+
+            while (j >= 0 && sorted[j] > key) {
+                sorted[j + 1] = sorted[j];
+                j = j - 1;
+            }
+            sorted[j + 1] = key;
+        }
+    }
+    static void bubble_sort( int A[ ], int n ) {
+        int temp;
+        for(int k = 0; k< n-1; k++) {
+
+            for(int i = 0; i < n-k-1; i++) {
+                if(A[ i ] > A[ i+1] ) {
+                    temp = A[ i ];
+                    A[ i ] = A[ i+1 ];
+                    A[ i + 1] = temp;
+                }
+            }
+        }
+    }
+
+}
+
