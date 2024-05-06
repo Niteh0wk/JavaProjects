7d4c12f6c472fb64a6f99f46ec863db1aef7631e
Niteh0wk <gerstl.philipp.24@gmail.com>
2024-05-06 11:40:42 +0200

    LinkedList added

--- 10-ObjektOrientiertesProgrammieren/LinkedList/src/List.java
+++ 10-ObjektOrientiertesProgrammieren/LinkedList/src/List.java
@@ -1,16 +1,65 @@
 public class List {
-    public static void main(String[] args) {
-        Node node1 = new Node(10);
-        Node node2 = new Node(20);
-        Node node3 = new Node(30);
 
-        node1.next = node2;
-        node2.next = node3;
+    private Node head;
+    private int size;
 
-        Node head = node1;
-        while (head != null){
-            System.out.println(head.value);
-            head = head.next;
+    void add (int value){
+        add(size, value);
+    }
+
+    void add(int index, int value){
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException("index=" + index);
+        }
+        if (index == 0){
+            head = new Node(head, value);
+        }else{
+            Node n = search(index-1);
+            n.next = new Node(n.next, value);
+        }
+        size++;
+    }
+
+    void get(int index){
+        Node n = search(index);
+        System.out.println("The value of index " + index + " = " + n.value);
+    }
+
+    void remove(int index){
+        Node n = search(index-1);
+        n.next = n.next.next;
+        size--;
+    }
+
+    Node search(int index){
+        Node n = head;
+        for (int i = 0; i < index; i++) {
+            n = n.next;
+        }
+        return n;
+    }
+
+    @Override
+    public String toString(){
+        Node n = head;
+        String s = "[";
+        for (int i = 0; i < size; i++) {
+            s += n.value + ", ";
+            n = n.next;
+        }
+        return s + "]";
+    }
+    public int size(){
+        return size;
+    }
+
+    class Node {
+        Node next;
+        int value;
+
+        public Node(Node next, int value) {
+            this.next = next;
+            this.value = value;
         }
     }
 }
--- 10-ObjektOrientiertesProgrammieren/LinkedList/src/Main.java
+++ 10-ObjektOrientiertesProgrammieren/LinkedList/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        List li = new List();
 
+        li.add(3);
+        li.add(5);
+        li.add(7);
+        li.add(9);
+        li.add(11);
+        li.add(13);
+
+        System.out.println(li);
+
+        li.add(5,12);
+
+        System.out.println(li + "with add");
+
+        li.remove(1);
+
+        System.out.println(li + "with remove");
+
+        li.get(3);
+
+        System.out.println("The size is = " + li.size());
     }
 }
