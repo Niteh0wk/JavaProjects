bd7d9f5daee3353efbecafcfb235c506694c9922
Niteh0wk <gerstl.philipp.24@gmail.com>
2024-05-08 14:07:58 +0200

    update

--- 10-ObjektOrientiertesProgrammieren/Stack/src/Main.java
+++ 10-ObjektOrientiertesProgrammieren/Stack/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Stack stack = new Stack();
+
+        stack.push(3);
+        stack.push(6);
+        stack.push(10);
+
+        stack.print();
+
+        System.out.println("Stack size = " + stack.size());
+
+        stack.pop(2);
+
+        System.out.println(stack.peek() + " Peek");
+
+        System.out.println(stack.pop() + " Pop single");
     }
 }
---
+++ 10-ObjektOrientiertesProgrammieren/Stack/src/Stack.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Stack {
+    ArrayList<Integer> stack = new ArrayList<>();
+
+
+    void push(int newElement){
+        stack.add(newElement);
+    }
+
+    public int size(){
+        return stack.size();
+    }
+    public int pop(){
+        if (stack.isEmpty()){
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        int temp = stack.getLast();
+        stack.remove(stack.size()-1);
+        return temp;
+    }
+    public int peek(){
+        if (stack.isEmpty()){
+            throw new IndexOutOfBoundsException("Stack is empty");
+        }
+        return stack.getLast();
+    }
+    public int[] pop(int n){
+        int[] temp = new int[n];
+        for (int i = 0; i < n; i++) {
+            temp[i] = stack.remove(stack.size()-1);
+            System.out.println("[" + temp[i] + "]");
+        }
+        return temp;
+    }
+
+    public void print(){
+        for(int i:stack){
+            System.out.println("[" + i + "]");
+        }
+    }
+}
