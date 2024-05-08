608a4dcf49812d67ebd772f260b69f59bb5642ce
Niteh0wk <gerstl.philipp.24@gmail.com>
2024-05-08 15:25:35 +0200

    update

---
+++ 10-ObjektOrientiertesProgrammieren/Queue/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+
+        queue.enqueue(1);
+        queue.enqueue(3);
+        queue.enqueue(5);
+        queue.enqueue(7);
+
+        queue.print();
+
+        System.out.println("Queue size = " + queue.size());
+
+        queue.dequeue(2);
+
+        System.out.println("Queue size = " + queue.size());
+        
+        queue.dequeue();
+
+        System.out.println();
+
+        queue.print();
+    }
+}
