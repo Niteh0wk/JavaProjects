75acde7f19ac4caeeedb51a7a442c7911660e68c
Niteh0wk <gerstl.philipp.24@gmail.com>
2024-05-21 13:15:23 +0200

    18 done

---
+++ 18-Arbeiten-mit-dateien/src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+     Person p1 = new Person();
+
+     try {
+         p1.createPerson("Philipp", "Gerstl","24.02.2003");
+     }catch (IOException e){
+         e.getCause();
+     }
+
+     try {
+         p1.readWithScanner();
+     }catch (FileNotFoundException e){
+         e.getCause();
+     }
+
+     try {
+         p1.readWithFiles();
+     }catch (IOException e){
+         e.getCause();
+     }
+
+    }
+}
---
+++ 18-Arbeiten-mit-dateien/src/main/java/Person.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class Person {
+    public void createPerson(String firstName, String lastName, String birthday) throws IOException {
+        FileWriter writer = new FileWriter("personen.cvs");
+        char[] content = (firstName + ", " + lastName + ", " + birthday).toCharArray();
+        writer.write(content);
+        writer.close();
+    }
+
+    public void readWithScanner() throws FileNotFoundException {
+        File personen = new File("personen.cvs");
+        Scanner fileScanner = new Scanner(personen);
+        while (fileScanner.hasNextLine()){
+            System.out.println(fileScanner.nextLine());
+        }
+        fileScanner.close();
+    }
+
+    public void readWithFiles() throws IOException {
+        List<String> content = Files.readAllLines(Paths.get("personen.cvs"));
+        for (String line : content){
+            System.out.println(line);
+        }
+    }
+}
